63a65138c014a3567b5ed105c873ea93fa6b95ca
to-som-ja <davidkoso252123@gmail.com>
2022-07-12 11:25:54 +0200

    Changed to percentual chance of passing the test

--- UnitTest.cs
+++ UnitTest.cs
@@ -3,25 +3,43 @@ namespace TestMock;
 [TestClass]
 public class UnitTest
 {   
+    Random rnd = new Random();
+
     [TestMethod]
     public void Test1()
     {
         string? test = Environment.GetEnvironmentVariable("TEST1",EnvironmentVariableTarget.Process);
-        bool pass = false ;
-        Assert.IsTrue(bool.TryParse(test, out pass) && pass);
+        int number;
+        int.TryParse(test, out number);
+        Console.WriteLine(number);
+        Assert.IsTrue(rnd.Next(100)<number);
     }
     [TestMethod]
     public void Test2()
     {
         string? test = Environment.GetEnvironmentVariable("TEST2",EnvironmentVariableTarget.Process);
-        bool pass = false ;
-        Assert.IsTrue(bool.TryParse(test, out pass) && pass);
+        int number;
+        int.TryParse(test, out number);
+        Console.WriteLine(number);
+        Assert.IsTrue(rnd.Next(100)<number);
     }
     [TestMethod]
     public void Test3()
     {
         string? test = Environment.GetEnvironmentVariable("TEST3",EnvironmentVariableTarget.Process);
-        bool pass = false ;
-        Assert.IsTrue(bool.TryParse(test, out pass) && pass);
+        int number;
+        int.TryParse(test, out number);
+        Console.WriteLine(number);
+        Assert.IsTrue(rnd.Next(100)<number);
+    }
+
+    [TestMethod]
+    public void Test4()
+    {   
+        string? test = Environment.GetEnvironmentVariable("TEST4",EnvironmentVariableTarget.Process);
+        int number;
+        int.TryParse(test, out number);
+        Console.WriteLine(number);
+        Assert.IsTrue(rnd.Next(100)<number);
     }
 }
