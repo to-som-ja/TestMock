d3c32e75618118454ed0c4e80ed9cf1bbc12c241
to-som-ja <davidkoso252123@gmail.com>
2022-07-12 09:22:51 +0200

    added writeline

--- UnitTest.cs
+++ UnitTest.cs
@@ -11,8 +11,10 @@ public class UnitTest
     [TestMethod]
     public void Test2()
     {
-        var user = Environment.GetEnvironmentVariable("user");
-        Assert.AreSame(expected:"user",actual:user);
+        var user = Environment.GetEnvironmentVariable("USER",EnvironmentVariableTarget.Process);
+        Console.WriteLine("user:");
+        Console.WriteLine(user);
+        Assert.AreSame(expected:"USER",actual:user);
     }
     [TestMethod]
     public void Test3()
